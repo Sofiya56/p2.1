cde11df9b84d00237c527dcb41d3962f2f4d8c05
Sofiya56 <sofiia.kornieieva@student.karazin.ua>
2025-02-19 13:25:47 +0200

    Create QUERY.SQL

---
+++ QUERY.SQL
@@ -0,0 +1,13 @@
+DELETE FROM contract_contract_details;
+
+DELETE FROM contract_details;
+
+DELETE FROM contracts;
+
+DELETE FROM products;
+
+DELETE FROM categories;
+
+DELETE FROM companies;
+
+DELETE FROM owners;
